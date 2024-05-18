c61447f3022c67ded269ebbdf66bf5360896eadc
Craftyawesome <craftyawesome.ak@gmail.com>
2024-05-18 01:23:55 -0400

    Update model.py

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ from feature_transformer import DoubleFeatureTransformerSlice
 torch.set_float32_matmul_precision("high")
 
 # 3 layer fully connected network
-L1 = 3072
+L1 = 256
 L2 = 15
 L3 = 32
 
